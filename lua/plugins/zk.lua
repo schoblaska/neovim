e7f36cec220075ebfd4d5ec26e582fc02a6b58a2
Joseph Schoblaska <jschoblaska@gmail.com>
2026-02-22 15:53:06 -0600

    zk slickness

--- lua/plugins/zk.lua
+++ lua/plugins/zk.lua
@@ -1,6 +1,17 @@
 return {
   "zk-org/zk-nvim",
   config = function()
+    local notebook = vim.env.ZK_NOTEBOOK_DIR
+    if notebook then
+      vim.api.nvim_create_autocmd("BufWritePost", {
+        pattern = notebook .. "/*.md",
+        callback = function(ev)
+          if vim.fn.fnamemodify(ev.file, ":t") == "index.md" then return end
+          vim.fn.jobstart({ "zk", "gen-index" }, { cwd = notebook })
+        end,
+      })
+    end
+
     require("zk").setup({
       picker = "telescope",
       lsp = {
@@ -16,7 +27,28 @@ return {
     })
   end,
   keys = {
-    { "<leader>nn", "<Cmd>ZkNew { title = vim.fn.input('Title: ') }<CR>", desc = "New note" },
+    { "<leader>nn", function()
+      local small_words = { "a", "an", "and", "as", "at", "but", "by", "for", "in", "nor", "of", "on", "or", "so", "the", "to", "up", "yet" }
+      local skip = {}
+      for _, w in ipairs(small_words) do skip[w] = true end
+
+      local function title_case(str)
+        local words = {}
+        for word in str:gmatch("%S+") do
+          if #words == 0 or not skip[word:lower()] then
+            table.insert(words, word:sub(1, 1):upper() .. word:sub(2):lower())
+          else
+            table.insert(words, word:lower())
+          end
+        end
+        return table.concat(words, " ")
+      end
+
+      local input = vim.fn.input("Title: ")
+      if input ~= "" then
+        require("zk.commands").get("ZkNew")({ title = title_case(input) })
+      end
+    end, desc = "New note" },
     { "<leader>nt", "<Cmd>ZkNotes { sort = { 'modified' } }<CR>", desc = "Find notes" },
     { "<leader>na", function()
       local input = vim.fn.input("Search: ")
